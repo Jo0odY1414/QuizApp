18ae108dead57a23b3abcfdc78d3398f881585b2
jo0ody1414 <amjad.98981@hotmail.com>
2017-07-29 19:08:55 +0300

    add Toast to displays the results of the quiz

--- app/src/main/java/com/example/android/quizapp/ResultActivity.java
+++ app/src/main/java/com/example/android/quizapp/ResultActivity.java
@@ -3,10 +3,12 @@ package com.example.android.quizapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -28,6 +30,10 @@ public class ResultActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the message as its text
         TextView textView = (TextView) findViewById(R.id.answer_result);
         textView.setText(message);
+
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.FILL_HORIZONTAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
 
         ImageView pic = (ImageView) findViewById(R.id.imageView);
 
